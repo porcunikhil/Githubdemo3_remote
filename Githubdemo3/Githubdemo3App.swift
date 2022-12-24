0fbc056ba31b3b250842418ef660206f54ea85fe
Nikhil Jain <jikhlin23@gmail.com>
2022-12-24 13:00:27 +0530

    Initial Commit

---
+++ Githubdemo3/Githubdemo3App.swift
@@ -0,0 +1,17 @@
+//
+//  Githubdemo3App.swift
+//  Githubdemo3
+//
+//  Created by Nikhil on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Githubdemo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
